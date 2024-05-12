2efd2780b7a65a8dda6fee2bcc5938b5d3d0dc41
Yuji-Terada <yuji070902@gmail.com>
2024-05-12 21:39:39 +0900

    モデル動かせた!!!!!

--- asset/js/script_map.js
+++ asset/js/script_map.js
@@ -27,18 +27,17 @@ renderer.shadowMap.enabled = true;
 const scene = new THREE.Scene();
 
 // 背景の設定
-// scene.background = new THREE.Color('#ffffff');
-
-scene.background = new THREE.CubeTextureLoader()
-  .setPath( './3d/background/' )
-  .load( [
-    'sky1.jpg',
-    'sky3.jpg',
-    'sky3.jpg',
-    'sky3.jpg',
-    'sky4.jpg',
-    'sky2.jpg'
-]);
+scene.background = new THREE.Color('#BBF2FF');
+// scene.background = new THREE.CubeTextureLoader()
+//   .setPath( './3d/background/' )
+//   .load( [
+//     'sky1.jpg',
+//     'sky3.jpg',
+//     'sky3.jpg',
+//     'sky3.jpg',
+//     'sky4.jpg',
+//     'sky2.jpg'
+// ]);
 
 // 見やすいようにヘルパー（網目）を設定
 // let gridHelper = new THREE.GridHelper();
@@ -52,15 +51,16 @@ camera.position.set(5, 2, 0);
 camera.lookAt(0, 0, 0);
 
 // ライトの作成
-const sun = new THREE.AmbientLight("#ffffff", 0.2);
+const sun = new THREE.AmbientLight("#ffffff", 0.3);
 // const light1 = new THREE.PointLight("#ffffff", 50, 20);
 // const light2 = new THREE.PointLight("#ffffff", 50, 20);
 // const light3 = new THREE.PointLight("#ffffff", 50, 20);
 // const light4 = new THREE.PointLight("#ffffff", 50, 20);
-const light1 = new THREE.SpotLight("#ffffff", 1, 20, Math.PI/180*120, 1, 0.05);
-const light2 = new THREE.SpotLight("#ffffff", 1, 20, Math.PI/180*120, 1, 0.05);
-const light3 = new THREE.SpotLight("#ffffff", 1, 20, Math.PI/180*120, 1, 0.05);
-const light4 = new THREE.SpotLight("#ffffff", 1, 20, Math.PI/180*120, 1, 0.05);
+// SpotLight([色], [強さ], [照射半径], [照射角度], ???, ???);
+const light1 = new THREE.SpotLight("#ffffff", 1.5, 40, Math.PI/180*120, 1, 0.05);
+const light2 = new THREE.SpotLight("#ffffff", 1.5, 40, Math.PI/180*120, 1, 0.05);
+const light3 = new THREE.SpotLight("#ffffff", 1.5, 40, Math.PI/180*120, 1, 0.05);
+const light4 = new THREE.SpotLight("#ffffff", 1.5, 40, Math.PI/180*120, 1, 0.05);
 light1.position.set(0, 10, 7);
 light2.position.set(0, 10, -7);
 light3.position.set(7, 10, 0);
@@ -78,13 +78,13 @@ scene.add(light4);
 // マウス制御
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableRotate = true; // カメラの回転を有効にする
-controls.maxPolarAngle = Math.PI / 180 * 75; // カメラの仰角の最小値を設定
-controls.minPolarAngle = Math.PI / 180 * 30; // カメラの仰角の最小値を設定
+// controls.maxPolarAngle = Math.PI / 180 * 80; // カメラの仰角の最小値を設定
+// controls.minPolarAngle = Math.PI / 180 * 25; // カメラの仰角の最小値を設定
 controls.minDistance = 3; // 最小距離を設定
 controls.maxDistance = 9; // 最大距離を設定
 controls.rotateSpeed = 0.3;
 controls.zoomSpeed = 0.2;
-controls.enablePan = false;     //true:パン操作可能,false:パン操作不可
+controls.enablePan = true;     //true:パン操作可能,false:パン操作不可
 controls.userPanSpeed = 1;   //パン速度????
 controls.minTargetRadius = 0;
 controls.enableDamping = true;  //なめらかな動きを可能に
@@ -92,22 +92,50 @@ controls.dampingFactor = 0.1;   //滑らかさの係数
 
 const controls2 = new FlyControls(camera, renderer.domElement);
 
+var y = 0;
 
 // 3Dモデルの読み込み
 const loader = new GLTFLoader();
-var model;
-loader.load('./3d/004/toin004.gltf', function (gltf) {
-    model = gltf.scene;
-    model.scale.set(0.05, 0.05, 0.05);
-    scene.add(model);
+
+const url = [
+  "./3d/004/toin004.gltf",
+  "./3d/004/test.gltf"
+];
+
+var model = {};
+var flag = false;
+
+loader.load(url[0], function (gltf) {
+  model.school = gltf.scene;
+  model.school.scale.set(0.07, 0.07, 0.07);
+  model.school.position.set(0, 0, 0);
+  scene.add(model.school);
+  flag = true;
+});
+
+loader.load(url[1], function (gltf) {
+  model.ball = gltf.scene;
+  model.ball.scale.set(0.07, 0.07, 0.07);
+  model.ball.position.set(0, 1.5, 0);
+  scene.add(model.ball);
+  flag = true;
 });
 
 // アニメーション
 function animate() {
-    controls.update();
-    // controls2.update();
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+
+  y += 0.08;
+
+  if(flag){
+    model.ball.position.set(Math.sin(y)*2, 1.5, Math.cos(y)*2);
+    model.school.rotation.y += 0.001;
+    console.log(model.school.rotation.y);
+  }
+    
+  controls.update();
+  // controls2.update();
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 // アニメーション実行
