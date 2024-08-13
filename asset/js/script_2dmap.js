da127a33be2354212fe14f8d63eebb68bbb9a8b3
Yuji-Terada <yuji070902@gmail.com>
2024-08-13 16:15:42 +0900

    2Dマップのファイルを追加した
    色々やってみたが...うまくいくか?

---
+++ asset/js/script_2dmap.js
@@ -0,0 +1,32 @@
+// var floor = 1;
+
+// $("#floorBtn1").click(function(){
+//   $("#map2d").addClass("floor1");
+//   $("#map2d").removeClass("floor2");
+//   $("#map2d").removeClass("floor3");
+//   // $(".floorBtn").removeClass("active");
+//   // $(this).addClass("active");
+//   $("#floorBtnBox>span").css("left", "0rem");
+// });
+
+// $("#floorBtn2").click(function(){
+//   $("#map2d").removeClass("floor1");
+//   $("#map2d").addClass("floor2");
+//   $("#map2d").removeClass("floor3");
+//   // $(".floorBtn").removeClass("active");
+//   // $(this).addClass("active");
+//   $("#floorBtnBox>span").css("left", "4rem");
+// });
+
+// $("#floorBtn3").click(function(){
+//   $("#map2d").removeClass("floor1");
+//   $("#map2d").removeClass("floor2");
+//   $("#map2d").addClass("floor3");
+//   // $(".floorBtn").removeClass("active");
+//   // $(this).addClass("active");
+//   $("#floorBtnBox>span").css("left", "8rem");
+// });
+
+
+
+
