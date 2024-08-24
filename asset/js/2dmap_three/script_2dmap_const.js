945e6644a0d78ed6d7bbb94adf49e62a9c9a5559
Yuji-Terada <yuji070902@gmail.com>
2024-08-25 00:15:03 +0900

    更新。

--- asset/js/2dmap_three/script_2dmap_const.js
+++ asset/js/2dmap_three/script_2dmap_const.js
@@ -5,13 +5,14 @@ const controlEnRotate = true;
 const controlEnPan = true;
 
 // カメラ操作の各スピード
-const controlSpRotate = 0.5;
-const controlSpZoom = 0.25;
-const controlSpPan = 0.5;
+const controlSpRotate = 0.1;
+const controlSpZoom = 1;
+const controlSpPan = 0.75;
 
 // カメラの仰角の最大値/最小値
-const controlMinAngle = 50;
-const controlMaxAngle = 45;
+// 仰角＝カメラの視線の線と地球を南北に貫く軸の角度の大きさ
+const controlMinAngle = Math.PI / 180 * 0;
+const controlMaxAngle = Math.PI / 180 * 0;
 
 // カメラの最大距離/最小距離
 const controlMinDistance = 3;
@@ -21,4 +22,4 @@ const controlMaxDistance = 9;
 const controlEnDamping = true;
 
 // カメラの滑らかな動きの係数
-const controlDampingFactor = 0.15;
+const controlDampingFactor = 0.25;
--- asset/js/2dmap_three/script_2dmap_main.js
+++ asset/js/2dmap_three/script_2dmap_main.js
@@ -10,7 +10,7 @@ let rot = 0;
 let mouseX = 0;
 
 const init = () => {
-  const canvasElement = document.querySelector('#myCanvas');
+  const canvasElement = document.querySelector('#mapCanvas');
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
       canvas: canvasElement
@@ -22,57 +22,71 @@ const init = () => {
   const scene = new THREE.Scene();
 
   // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  // PerspectiveCamera(画角, アスペクト比＝縦横比, 描画開始距離, 描画終了距離);
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.0000001, 10000000);
 
-  // 球体を作成
-  const geometry = new THREE.SphereGeometry(350, test2, 30);
-
-  // // マテリアルにテクスチャーを設定
-  const material = new THREE.MeshStandardMaterial({
-      map: new THREE.TextureLoader().load('earthmap1k.jpeg')
-  });
-
-  // メッシュを作成
-  const earthMesh = new THREE.Mesh(geometry, material);
-
-  // 3D空間にメッシュを追加
-  scene.add(earthMesh);
+  // 平面を作成・マテリアルにテクスチャーを設定
+  const planeGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+  const planeMaterial = new THREE.MeshToonMaterial({map: new THREE.TextureLoader().load('earthmap1k.jpeg'), side: THREE.DoubleSide}) 
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.position.set(0, 0, 0);
+  plane.rotation.set(Math.PI / 180 * -90, 0, 0);
+  scene.add(plane);
 
   // 平行光源
-  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-  directionalLight.position.set(1, 1, 1);
-
-  // シーンに追加
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(0, 50, 0);
   scene.add(directionalLight);
-
-  // マウス座標はマウスが動いた時のみ取得できる
-  document.addEventListener("mousemove", (event) => {
-    mouseX = event.pageX;
-  });
 
   // カメラコントローラーを作成
   const controls = new THREE.OrbitControls(camera, canvasElement);
 
-  // 滑らかにカメラコントローラーを制御する
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.2;
+  // 各種カメラ設定（詳細はscript_2dmap_const.jsを参照）
+  controls.enableRotate = controlEnRotate;
+  controls.enablePan = controlEnPan;
+  controls.rotateSpeed = controlSpRotate;
+  controls.zoomSpeed = controlSpZoom;
+  controls.panSpeed = controlSpPan;
+  controls.minPolarAngle = controlMinAngle;
+  controls.maxPolarAngle = controlMaxAngle;
+  // ズームの深度はcontrols.object.position.yで取得可能（カメラのy座標）
+  // controls.minDistance = controlMinDistance;
+  // controls.maxDistance = controlMaxDistance;
+  controls.enableDamping = controlEnDamping;
+  controls.dampingFactor = controlDampingFactor;
+  controls.object.position.y = Math.exp(7);
 
+  
   // 毎フレーム時に実行されるループイベントです
   const tick = () => {
-    // マウスの位置に応じて角度を設定
-    // マウスのX座標がステージの幅の何%の位置にあるか調べてそれを360度で乗算する
-    const targetRot = (mouseX / window.innerWidth) * 360;
     // 原点方向を見つめる
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    // camera.lookAt(new THREE.Vector3(0, 0, 0));
+    
     // カメラコントローラーを更新
     controls.update();
 
-    // 地球は常に回転させておく
-    // earthMesh.rotation.y += 0.01;
     // レンダリング
     renderer.render(scene, camera);
-    
+
     requestAnimationFrame(tick);
+
+    console.log(controls.object.position.x);
+    console.log(Math.log(controls.object.position.y));
+
+    // ズームアウトしすぎを解消
+    if(Math.log(controls.object.position.y) > 8.5){
+      controls.object.position.y = Math.exp(8.5);
+    }
+    // ズームインしすぎを解消
+    else if(Math.log(controls.object.position.y) < 0){
+      controls.object.position.y = Math.exp(0);
+    }
+
+    // 移動範囲を制限
+    if(controls.object.position.x > 1500){
+      console.log(controls.object.position.x);
+      console.log(controls.object.position.x+1);
+    }
   }
 
   tick();
@@ -98,9 +112,9 @@ const init = () => {
     camera.aspect = width / height;
     //camera.position.set(1000, 1000, 2000);
     if (window.matchMedia('(max-width: 767px)').matches) {
-      camera.position.set(1000, 1000, 2000);
+      // camera.position.set(1000, 1000, 2000);
     } else if (window.matchMedia('(min-width:768px)').matches) {
-      camera.position.set(0, 0, +1300);
+      // camera.position.set(0, 0, +1300);
     }
     camera.updateProjectionMatrix();
   }
