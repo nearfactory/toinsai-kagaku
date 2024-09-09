11e547d57d8587503acd2efb1b1e507517200529
Yuji-Terada <yuji070902@gmail.com>
2024-09-10 00:45:49 +0900

    更新。
    生徒会本部での表示用ページを作成

---
+++ statusCheck/asset/script.js
@@ -0,0 +1,83 @@
+var api_url = 'https://script.google.com/macros/s/AKfycbyTsJwu8dszcX7lSbERPr_YXRewyPVNimgomaDhn2k3jeTHZbXM3uNlwrd2kteykRfIdQ/exec'; //生成したAPIのURLを指定
+fetch(api_url)
+.then(function (fetch_data) {
+  return fetch_data.json();
+})
+.then(function (json) {
+  console.log(json);
+  for (var i in json) {
+    if(json[i].state == "混雑"){
+      $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+      $("#waitStatusStatus" + String(Number(i)+1)).addClass("high");
+      $("#waitStatusClass" + String(Number(i)+1)).addClass("high");
+      $("#waitStatusStatus" + String(Number(i)+1)).html("混雑");
+    }
+    else if(json[i].state == "普通"){
+      $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+      $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+      $("#waitStatusStatus" + String(Number(i)+1)).addClass("middle");
+      $("#waitStatusClass" + String(Number(i)+1)).addClass("middle");
+      $("#waitStatusStatus" + String(Number(i)+1)).html("普通");
+    }
+    else if(json[i].state == "空いている"){
+      $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+      $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+      $("#waitStatusStatus" + String(Number(i)+1)).addClass("low");
+      $("#waitStatusClass" + String(Number(i)+1)).addClass("low");
+      $("#waitStatusStatus" + String(Number(i)+1)).html("空き");
+    }
+    else{
+      $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+      $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+      $("#waitStatusStatus" + String(Number(i)+1)).html("取得中");
+    }
+    
+    if(i > 8){
+      $("#stockStatusItem1Stock" + String(Number(i)-8)).html(json[i].item1);
+      $("#stockStatusItem2Stock" + String(Number(i)-8)).html(json[i].item2);
+    }
+  }
+});
+
+stateUpdate = setInterval(function(){
+  fetch(api_url)
+  .then(function (fetch_data) {
+    return fetch_data.json();
+  })
+  .then(function (json) {
+    console.log(json);
+    for (var i in json) {
+      if(json[i].state == "混雑"){
+        $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+        $("#waitStatusStatus" + String(Number(i)+1)).addClass("high");
+        $("#waitStatusClass" + String(Number(i)+1)).addClass("high");
+        $("#waitStatusStatus" + String(Number(i)+1)).html("混雑");
+      }
+      else if(json[i].state == "普通"){
+        $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+        $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+        $("#waitStatusStatus" + String(Number(i)+1)).addClass("middle");
+        $("#waitStatusClass" + String(Number(i)+1)).addClass("middle");
+        $("#waitStatusStatus" + String(Number(i)+1)).html("普通");
+      }
+      else if(json[i].state == "空いている"){
+        $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+        $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+        $("#waitStatusStatus" + String(Number(i)+1)).addClass("low");
+        $("#waitStatusClass" + String(Number(i)+1)).addClass("low");
+        $("#waitStatusStatus" + String(Number(i)+1)).html("空き");
+      }
+      else{
+        $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
+        $("#waitStatusClass" + String(Number(i)+1)).removeClass();
+        $("#waitStatusStatus" + String(Number(i)+1)).html("取得中");
+      }
+    
+      if(i > 8){
+        $("#stockStatusItem1Stock" + String(Number(i)-8)).html(json[i].item1);
+        $("#stockStatusItem2Stock" + String(Number(i)-8)).html(json[i].item2);
+      }
+    }
+  });
+}
+, 10000);
