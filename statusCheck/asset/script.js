f73069220b3ea1abdf4c5d76d6d87afa458ea54b
Yuji-Terada <yuji070902@gmail.com>
2024-09-11 22:46:57 +0900

    更新。

--- statusCheck/asset/script.js
+++ statusCheck/asset/script.js
@@ -1,10 +1,10 @@
+// 混雑状況の取得
 var api_url = 'https://script.google.com/macros/s/AKfycbySfCC1bco8J_rok4uhw6vZYsz527CK6KsswCkpsASiN9GzbG96Jyc5yV8uR5ibjc2S6Q/exec'; //生成したAPIのURLを指定
 fetch(api_url)
 .then(function (fetch_data) {
   return fetch_data.json();
 })
 .then(function (json) {
-  console.log(json);
   for (var i in json) {
     if(json[i].state == "混雑"){
       $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
@@ -45,7 +45,6 @@ stateUpdate = setInterval(function(){
     return fetch_data.json();
   })
   .then(function (json) {
-    console.log(json);
     for (var i in json) {
       if(json[i].state == "混雑"){
         $("#waitStatusStatus" + String(Number(i)+1)).removeClass();
@@ -82,6 +81,12 @@ stateUpdate = setInterval(function(){
 }
 , 10000);
 
+
+
+
+
+
+// 配信画面のYouTube映像を再生
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -92,7 +97,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     width: '100%',
     height: '100%',
-    videoId: 'AZpE2fziy00',
+    videoId: 'fVLbzHNeGZ8', // YouTube映像の動画IDをここに貼付
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -100,14 +105,10 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
@@ -118,3 +119,58 @@ function onPlayerStateChange(event) {
 function stopVideo() {
   player.stopVideo();
 }
+
+
+
+
+
+// ニュースの読み込み
+// スケジュールをCSVファイルから取得
+const newsCSV = '../asset/csv/news.csv'; // ここにCSVファイルのURLを入力する
+
+// promiseを使用して完了まで待機するシステムにした
+function fetchCSV(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(`Failed to fetch CSV: ${xhr.status}`);
+        }
+      }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  });
+}
+
+// 読み込んだCSVファイルを二次元配列に組みなおす
+function parseCSV(csvText) {
+  const lines = csvText.split(/\r\n|\n/);
+  const data = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const currentLine = lines[i].split(',');
+    if (currentLine.length > 0) {
+      data.push(currentLine);
+    }
+  }
+
+  return data;
+}
+
+function initNews(){
+  Promise.all([
+    fetchCSV(newsCSV)
+  ]).then(([csvText]) => {
+    
+    var news = parseCSV(csvText);
+    $("#newsTitle").html(news[news.length-1][2]);
+    $("#newsContent1").html(news[news.length-1][5]);
+    $("#newsContent2").html(news[news.length-1][5]);
+  });
+}
+initNews();
+setInterval(initNews, 50);
